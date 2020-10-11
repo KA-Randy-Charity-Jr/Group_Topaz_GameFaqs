65987fc393c65e9d74beb807a91cdcadc69cd445
Michael DeMory <mjdemory2891@gmail.com>
2020-10-11 16:11:28 -0400

    user profile edit form done, user profile view and user edit profile view constructed. URLs, html need to be constructed, and all functionality need to be tested.

--- user_faq/models.py
+++ user_faq/models.py
@@ -8,6 +8,8 @@ class GamefaqUser(AbstractUser):
     displayname = models.CharField(max_length=80)
     bio = models.TextField(blank=True, null=True)
     preferred_systems = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+
 
     def __str__(self):
         return self.username
--- user_faq/views.py
+++ user_faq/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login
 from authentication.views import login_view
 from user_faq.models import GamefaqUser
 from user_faq.forms import SignupForm
+from user_faq.forms import GamefaqUserForm
 
 # Create your views here.
 
@@ -31,3 +32,33 @@ class signup_view(TemplateView):
                 )
                 login(request, new_user)
                 return HttpResponseRedirect(reverse('homepage'))
+
+def user_profile_view(request, user_id):
+    html = 'user_profile.html'
+    user_detail = GamefaqUser.objects.filter(id=user_id).first()
+    return render(request, html, {'profile':user_detail})
+
+
+class edit_user_profile_view(TemplateView):
+    def get(self, request):
+        data = {
+            "displayname": GamefaqUser.displayname,
+            "bio": GamefaqUser.bio,
+            "preferred_systems": GamefaqUser.preferred_systems,
+            "email": GamefaqUser.email
+        }
+        form = GamefaqUserForm(initial=data)
+        return render(request, "basic.html", {"form": form})
+
+    def post(self, request, user_id):
+        faq_user = GamefaqUser.objects.get(id=user_id)
+        if request.method == "POST":
+            form = GamefaqUserForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                faq_user.displayname = data["displayname"]
+                faq_user.bio = data["bio"]
+                faq_user.preferred_systems = data["preferred_systems"]
+                faq_user.email = data["email"]
+                faq_user.save()
+                return HttpResponseRedirect(reverse('ticket_detail', args=[faq_user.id]))
