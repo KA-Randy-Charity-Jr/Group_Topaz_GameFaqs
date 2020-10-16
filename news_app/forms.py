a6943e70902190ed87b2079172974c51aa0e0e39
rakechj <rakechj2@outlook.com>
2020-10-16 07:10:24 -0400

    fixed news posting other minor tweaks

--- news_app/forms.py
+++ news_app/forms.py
@@ -5,7 +5,6 @@ from game_app.models import Game
 
 class NewNewspost(forms.Form):
     title = forms.CharField(max_length=150)
-    game = forms.ModelChoiceField(queryset=Game.objects.all(),required=False)
     body = forms.CharField(widget=forms.Textarea)
 
     
--- news_app/views.py
+++ news_app/views.py
@@ -3,22 +3,25 @@ from django.views.generic import TemplateView
 from news_app.forms import NewNewspost
 from news_app.models import Newspost
 from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
+from game_app.models import Game
 # Create your views here.
 
 class Newspostform(TemplateView):
-    def get(self, request):
+    def get(self, request,gameid):
         f = NewNewspost()
+        game = Game.objects.get(id=gameid)
         return render(request,"form.html",{"f":f})
-    def post(self, request):
+    def post(self, request,gameid):
         if request.method == "POST":
             form = NewNewspost(request.POST)
+            game= Game.objects.get(id=gameid)
             if form.is_valid():
                 data = form.cleaned_data
                 Newspost.objects.create(
                     title=data.get('title'),
                     body=data.get('body'),
                     author=request.user,
-                    game=data.get('game')
+                    game=game
                 )
                 
                 return HttpResponseRedirect(reverse('home'))
