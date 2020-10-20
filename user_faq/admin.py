0b6ebbf7a8019f55683e3a8b7813bd124655777c
rakechj <rakechj2@outlook.com>
2020-10-20 11:59:56 -0400

    added oof

--- user_faq/admin.py
+++ user_faq/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from user_faq.models import GamefaqUser
+from user_faq.models import User_Comments
 # Register your models here.
 
 admin.site.register(GamefaqUser)
+admin.site.register(User_Comments)
